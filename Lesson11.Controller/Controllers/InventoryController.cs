89a2bec4ec362541155a61b06523c2a8f6c683d6
pavka <pavka@DESKTOP-8I6Q8OI>
2023-11-07 11:23:56 +0300

    Добавьте файлы проекта.

---
+++ Lesson11.Controller/Controllers/InventoryController.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/inventory")]
+[ApiController]
+public class InventoryController : ControllerBase
+{
+    private List<Product> inventory = new List<Product>();
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Product>> GetProducts()
+    {
+        return inventory;
+    }
+
+    [HttpPost]
+    public ActionResult<Product> AddProduct(Product product)
+    {
+        inventory.Add(product);
+        return product;
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Product> UpdateProduct(int id, Product updatedProduct)
+    {
+        var existingProduct = inventory.FirstOrDefault(p => p.Id == id);
+
+        if (existingProduct == null)
+        {
+            return NotFound();
+        }
+
+        existingProduct.Name = updatedProduct.Name;
+        existingProduct.Price = updatedProduct.Price;
+        existingProduct.Quantity = updatedProduct.Quantity;
+
+        return existingProduct;
+    }
+}
---
+++ Lesson11.Controller/Controllers/InventorySumController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/summary")]
+[ApiController]
+public class InventorySummaryController : ControllerBase
+{
+    private List<Product> inventory = new List<Product>();
+
+    [HttpGet("total")]
+    public ActionResult<decimal> GetTotalInventoryValue()
+    {
+        decimal totalValue = inventory.Sum(product => product.Price * product.Quantity);
+        return totalValue;
+    }
+
+    [HttpGet("byCategory/{categoryId}")]
+    public ActionResult<decimal> GetTotalValueByCategory(int categoryId)
+    {
+        decimal totalValue = inventory.Where(product => product.CategoryId == categoryId)
+                                      .Sum(product => product.Price * product.Quantity);
+        return totalValue;
+    }
+}
